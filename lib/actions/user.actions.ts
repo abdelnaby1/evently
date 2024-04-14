de06f766d6caeee13bb4adca9a3c4862cee02756
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2024-04-14 18:00:20 +0200

    fix clerk igoneroutes

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -11,7 +11,7 @@ export const createUser = async (user: CreateUserParams) => {
   try {
     await connectToDB();
     const newUser = await User.create(user);
-    return JSON.parse(JSON.stringify(user));
+    return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     handleError(error);
   }
